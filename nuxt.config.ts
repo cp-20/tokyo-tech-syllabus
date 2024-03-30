6f42db7031c557ffeb37b503f72282ac94a0b88c
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2024-03-31 05:31:25 +0900

    OGPを設定 (close #2)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,66 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: '東工大シラバス - Tokyo Tech Syllabus',
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: '東工大の講義を爆速で検索できるサイト',
+        },
+        { name: 'format-detection', content: 'telephone=no' },
+        { name: 'msapplication-TileColor', content: '#ffffff' },
+        { name: 'theme-color', content: '#ffffff' },
+        {
+          property: 'og:title',
+          content: '東工大シラバス - Tokyo Tech Syllabus',
+        },
+        {
+          property: 'og:description',
+          content: '東工大の講義を爆速で検索できるサイト',
+        },
+        {
+          property: 'og:image',
+          content: 'https://tokyo-tech-syllabus.cp20.dev/ogp.png',
+        },
+        {
+          property: 'og:url',
+          content: 'https://tokyo-tech-syllabus.cp20.dev/',
+        },
+        { property: 'og:type', content: 'website' },
+        { name: 'twitter:card', content: 'summary_large_image' },
+        {
+          name: 'twitter:title',
+          content: '東工大シラバス - Tokyo Tech Syllabus',
+        },
+        {
+          name: 'twitter:description',
+          content: '東工大の講義を爆速で検索できるサイト',
+        },
+        {
+          name: 'twitter:image',
+          content: 'https://tokyo-tech-syllabus.cp20.dev/ogp.png',
+        },
+        {
+          name: 'twitter:url',
+          content: 'https://tokyo-tech-syllabus.cp20.dev/',
+        },
+      ],
+      link: [
+        { rel: 'icon', href: '/favicon.ico', sizes: '32x32' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/icon.svg' },
+        {
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          href: '/apple-touch-icon.png',
+        },
+        { rel: 'manifest', href: '/manifest.webmanifest' },
+      ],
+    },
+  },
   devtools: { enabled: true },
 
   nitro: {
--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,7 @@ const searchQuery = ref<SearchQuery>({});
     <div class="mb-8 mt-8">
 
       <h1 class="text-3xl font-bold m-0">東工大シラバス</h1>
-      <p class="text-slate-400 m-0 mt-2">TOKYO TECH OCWを爆速にしたシラバスです</p>
+      <p class="text-slate-400 m-0 mt-2">東工大の講義を爆速で検索できるサイト</p>
       <p class="text-slate-600">このサイト上の全ての情報は <CustomLink href="https://www.ocw.titech.ac.jp/">TOKYO TECH OCW
         </CustomLink> から提供されており
         <CustomLink href="https://creativecommons.org/licenses/by-nc-sa/2.1/jp/deed.ja">CC-BY-NC-SA 2.1 JP
