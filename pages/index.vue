72cc94b2a7d937bc6f4c96e71ebf9a805cf26f77
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2024-03-31 04:52:50 +0900

    close #1

--- pages/index.vue
+++ pages/index.vue
@@ -12,6 +12,11 @@ const searchQuery = ref<SearchQuery>({});
 
       <h1 class="text-3xl font-bold m-0">東工大シラバス</h1>
       <p class="text-slate-400 m-0 mt-2">TOKYO TECH OCWを爆速にしたシラバスです</p>
+      <p class="text-slate-600">このサイト上の全ての情報は <CustomLink href="https://www.ocw.titech.ac.jp/">TOKYO TECH OCW
+        </CustomLink> から提供されており
+        <CustomLink href="https://creativecommons.org/licenses/by-nc-sa/2.1/jp/deed.ja">CC-BY-NC-SA 2.1 JP
+          DEED</CustomLink> の元で自由に利用可能です
+      </p>
     </div>
     <SearchOptions v-model="searchQuery" />
     <LectureList :query="searchQuery" />
