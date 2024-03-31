9e8549025b9fcfaae9bf0b9568c61c1684fad63a
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2024-03-31 22:03:19 +0900

    refactoring

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { ref, watch } from 'vue';
+import { ref } from 'vue';
 import type { SearchQuery } from '../schema/searchQuery';
 
 const searchQuery = ref<SearchQuery>({});
